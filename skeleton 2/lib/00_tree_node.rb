9059fe0b4542d94fec995f4ab08af529c17c0f79
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2021-12-17 08:37:39 -0800

    add parent

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,44 @@
 class PolyTreeNode
+    attr_reader :parent, :value, :children
+    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent)
+        if @parent != nil
+            @parent.children.delete(self)
+        end
+
+        @parent = parent
+        @parent.children << self if parent != nil
+    end
+
+    def add_child
+        
+    end
+
+
+
+
+
 
 end
+
+n1 = PolyTreeNode.new("root1")
+n2 = PolyTreeNode.new("root2")
+n3 = PolyTreeNode.new("root3")
+
+# connect n3 to n1
+n3.parent = n1
+# connect n3 to n2
+n3.parent = n2
+
+# this should work
+raise "Bad parent=!" unless n3.parent == n2
+raise "Bad parent=!" unless n2.children == [n3]
+
+# this probably doesn't
+raise "Bad parent=!" unless n1.children == []
